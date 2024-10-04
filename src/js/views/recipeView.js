c0090001662a9def7fd8cd56de6e8e66ac63e531
mahibargal <maheshb@easyecom.io>
2024-10-04 13:13:26 +0530

    fraction removed.

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+// import { Fraction } from 'fractional';
 import { View } from './view.js';
 class RecipeView extends View {
   _data;
